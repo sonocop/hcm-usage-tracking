408505f355c0b199f1f2291773e711c6270186ac
ashley wardell <ashw1984@hotmail.co.uk>
2018-11-18 20:22:21 +0000

    added some global error handling

--- Access.HCM.UsageTracking/Startup.cs
+++ Access.HCM.UsageTracking/Startup.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
 using Access.Data;
 using Access.Data.Dapper;
@@ -9,9 +12,11 @@ using Access.HCM.UsageTracking.Logic;
 using Access.HCM.UsageTracking.Model.Data;
 using Access.HCM.UsageTracking.Model.Logic;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -49,6 +54,42 @@ namespace Access.HCM.UsageTracking
             {
                 app.UseHsts();
             }
+            app.UseExceptionHandler(errorApp =>
+            {
+                errorApp.Run(async context =>
+                {
+                    var errorFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    var exception = errorFeature.Error;
+
+                    // the IsTrusted() extension method doesn't exist and
+                    // you should implement your own as you may want to interpret it differently
+                    // i.e. based on the current principal
+
+                    var problemDetails = new ProblemDetails
+                    {
+                        Instance = $"urn:myorganization:error:{Guid.NewGuid()}"
+                    };
+
+                    if (exception is BadHttpRequestException badHttpRequestException)
+                    {
+                        problemDetails.Title = "Invalid request";
+                        problemDetails.Status = (int)typeof(BadHttpRequestException).GetProperty("StatusCode",
+                            BindingFlags.NonPublic | BindingFlags.Instance).GetValue(badHttpRequestException);
+                        problemDetails.Detail = badHttpRequestException.Message;
+                    }
+                    else
+                    {
+                        problemDetails.Title = "An unexpected error occurred!";
+                        problemDetails.Status = 500;
+                        problemDetails.Detail = exception.Message.ToString();
+                    }
+
+                    //TOOD: log the exception etc..
+
+                    context.Response.StatusCode = problemDetails.Status.Value;
+                    await context.Response.Body.WriteAsync(Encoding.UTF8.GetBytes(problemDetails.Detail));
+                });
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
