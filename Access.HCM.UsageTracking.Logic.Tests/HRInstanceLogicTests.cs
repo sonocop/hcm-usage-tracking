880818c73d9f1a3650bbd1345b55505cf2a1f9e4
ashley wardell <ashw1984@hotmail.co.uk>
2018-11-19 21:32:23 +0000

    test tests

---
+++ Access.HCM.UsageTracking.Logic.Tests/HRInstanceLogicTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Access.HCM.UsageTracking.Logic.Tests
+{
+    public class HRInstanceLogicTests
+    {
+        [Fact]
+        public void PassTest() {
+            Assert.True(true);
+        }
+        [Fact]
+        public void FailTest()
+        {
+            Assert.True(false);
+        }
+    }
+}
